c3d1e01df2f79549cdda3dc7009ec7a670898ee1
Jiana Wu <jiana.wu99@gmail.com>
2020-02-22 15:13:58 +1000

    connection

--- ShoppingCart/Infrastructure/ShoppingCartContext.cs
+++ ShoppingCart/Infrastructure/ShoppingCartContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using ShoppingCart.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,5 +13,6 @@ namespace ShoppingCart.Infrastructure
                 : base(options)
         {
         }
+        public DbSet<Page> Pages { get; set; }
     }
 }
